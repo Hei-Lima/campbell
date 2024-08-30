8b80bed945b8554e1a3a2b09940cd6183c3e2bd8
hei-lima <heitor.lipeixoto@gmail.com>
2024-08-29 22:05:29 -0300

    Added create screens (iso and node)

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,26 @@
-from flask import render_template, current_app as app
-from app.utils import connect_to_proxmox, list_vms
+from flask import render_template, url_for, request, jsonify, current_app as app
+from app.utils import *
 
 proxmox = connect_to_proxmox()
 
 @app.route('/')
 def index():
-    vms = list_vms()
+    vms = list_vms(proxmox)
     print(vms)
     return render_template('index.html', vms=vms)
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/create')
+def create():
+    nodes = list_nodes(proxmox)
+    return render_template('create.html', nodes=nodes)
+
+@app.route('/create/iso', methods=['GET', 'POST'])
+def iso():
+    if request.method == "POST":
+            node = request.form.get('node_name')
+            isos = list_isos(proxmox, node)
+    return render_template('createiso.html', isos=isos)
--- app/utils.py
+++ app/utils.py
@@ -9,10 +9,18 @@ def connect_to_proxmox():
     )
     return proxmox
 
-proxmox = connect_to_proxmox()
-
-def list_vms():
+def list_vms(proxmox):
     nodes = proxmox.nodes.get()
     for node in nodes:
         vms = proxmox.nodes(node['node']).qemu.get()
     return vms
+
+# TODO - Arrumar o storage name de forma dinamica. Não quero adicionar mais um passo sem usar AJAX.
+def list_isos(proxmox, node, storage_name="local"):
+    isos = proxmox.nodes(node).storage(storage_name).content.get(content='iso')
+    return [iso['volid'] for iso in isos]
+    
+
+def list_nodes(proxmox):
+    nodes = proxmox.nodes.get()
+    return [node['node'] for node in nodes]
